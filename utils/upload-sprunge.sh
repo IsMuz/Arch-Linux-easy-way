04c902d29ed9298deaeb0483f5ed82612b188116
Sergey M <tz4678@gmail.com>
2020-07-25 17:00:31 +0300

    new version

--- utils/upload-sprunge.sh
+++ utils/upload-sprunge.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 # Author: Sergey M <tz4678@gmail.com>
 # Загружает сниппет на аналог Pastebin'а
-cat ./arch-install.sh | curl -sF 'sprunge=<-' http://sprunge.us
+set -e
+cd `dirname "$0"`
+cat ../arch-install.sh | curl -sF 'sprunge=<-' http://sprunge.us
